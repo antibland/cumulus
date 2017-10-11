cf008cae845b2a7720318e8b108ae16a854d771e
Andy Hoffman <antibland@gmail.com>
2017-10-11 11:51:56 -0700

    Performance optimization gynastics

--- prod/main.js
+++ prod/main.js
@@ -1,4 +1,6 @@
 (function() {
+  "use strict";
+
   function testWebP(callback) {
     var webP = new Image();
     webP.src = 'data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA';
